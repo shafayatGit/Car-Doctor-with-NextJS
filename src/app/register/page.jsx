eb8dd497410dcad9e1c07f6175536d8febb121be
shafayatGit <shafayathossain.ielts@gmail.com>
2025-08-08 15:17:47 +0600

    auth implemented

--- src/app/register/page.jsx
+++ src/app/register/page.jsx
@@ -8,7 +8,6 @@ export default function RegisterPage() {
     <>
       <h1 className="text-3xl font-bold text-center my-8">Register</h1>
       <section className="container mx-auto grid grid-cols-12">
-        {/* Left Section */}
         <div className="col-span-12 md:col-span-6 flex justify-center items-center">
           <Image
             className="hidden md:block"
